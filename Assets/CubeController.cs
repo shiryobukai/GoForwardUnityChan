f4d6b5f32a7fb10946b3c6c0e0fc2882f6e09f1b
shiryobukai <s1513087.logic@gmail.com>
2018-06-22 21:38:30 +0900

    add sound effect

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -8,9 +8,12 @@ public class CubeController : MonoBehaviour {
 
         // 消滅位置
         private float deadLine = -10;
+
+    private AudioSource sound;
+
 	// Use this for initialization
 	void Start () {
-		
+		sound = GetComponent<AudioSource> ();	
 	}
 	
 	// Update is called once per frame
@@ -22,5 +25,11 @@ public class CubeController : MonoBehaviour {
                 if (transform.position.x < this.deadLine){
                         Destroy (gameObject);
                 }
+	}
+
+	//衝突時に呼ばれる関数
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag != "UnityChanTag")
+			sound.PlayOneShot(sound.clip);
 	}
 }
